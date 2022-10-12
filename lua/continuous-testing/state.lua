8c2ac491278bbcea0a7d6b16a0cca3cde6f5d13f
Aaron Hallaert <37899722+aaronhallaert@users.noreply.github.com>
2022-10-12 11:03:48 +0200

    Fix attached state (#7)

--- lua/continuous-testing/state.lua
+++ lua/continuous-testing/state.lua
@@ -1,5 +1,4 @@
 -- {[bufnr] : { ...state... }}
-
 local global_test_state = {}
 
 local M = {}
@@ -10,6 +9,25 @@ end
 
 M.update_state = function(bufnr, test_state)
     global_test_state[bufnr] = test_state
+end
+
+M.is_attached = function(bufnr)
+    for k, _ in pairs(global_test_state) do
+        -- buffer is in keys and value is not nil
+        if bufnr == k and global_test_state[k] ~= nil then
+            return true
+        end
+    end
+
+    return false
+end
+
+M.detach = function(bufnr)
+    global_test_state[bufnr] = nil
+end
+
+M.attach = function(bufnr)
+    global_test_state[bufnr] = {}
 end
 
 M.attached_tests = function()
