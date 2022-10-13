ae620caad834e2e509577cfc87be3f29c0c1e83d
Aaron Hallaert <37899722+aaronhallaert@users.noreply.github.com>
2022-10-13 18:52:28 +0200

    Handle sigterm perform test of last save (#9)

--- lua/continuous-testing/notify.lua
+++ lua/continuous-testing/notify.lua
@@ -1,11 +1,19 @@
 local NOTIFY_TITLE = "ContinuousTesting.nvim"
 
-return function(content, log_level)
-    local config = require("continuous-testing.config").get_config()
+return function(
+    content,
+    log_level,
+    --[[optional]]
+    title
+)
+    if title == nil or title == "" then
+        title = NOTIFY_TITLE
+    end
 
+    local config = require("continuous-testing.config").get_config()
     if not config.notify then
         return
     end
 
-    vim.notify(content, log_level, { title = NOTIFY_TITLE })
+    vim.notify(content, log_level, { title = title })
 end
