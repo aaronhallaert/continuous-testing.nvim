91a330eceab607d3ccf030aa094f6e52b90176e6
Aaron Hallaert <37899722+aaronhallaert@users.noreply.github.com>
2022-10-19 12:16:37 +0200

    Fix configs (#17)

--- lua/continuous-testing/config.lua
+++ lua/continuous-testing/config.lua
@@ -5,7 +5,7 @@ local config = {}
 
 local DEFAULT_CONFIG = {
     notify = false,
-    run_tests_on_setup = false,
+    run_tests_on_setup = true,
 }
 
 local set_default_values = function()
@@ -23,25 +23,34 @@ M.set_user_specific_config = function(user_config)
 end
 
 M.get_config = function()
-    local merged_config = table_util.deepcopy_table(config.framework_setup)
+    local result = {}
+    -- start with default config
+    local default_c = table_util.deepcopy_table(DEFAULT_CONFIG)
+    table_util.merge_table(result, default_c)
+
+    -- merge the framework configs (general and project)
+    local framework_config = {}
+
+    local general_framework_config =
+        table_util.deepcopy_table(config.framework_setup)
+    if general_framework_config ~= nil then
+        table_util.merge_table(framework_config, general_framework_config)
+    end
 
     local project_framework_config =
         table_util.deepcopy_table(config.project_override[vim.fn.getcwd()])
-
     if project_framework_config ~= nil then
-        for k, v in pairs(project_framework_config) do
-            merged_config[k] = v
-        end
+        table_util.merge_table(framework_config, project_framework_config)
     end
+    table_util.merge_table(result, framework_config)
 
-    for k, v in pairs(config) do
-        merged_config[k] = v
-    end
+    -- merge all other variables and remove the framework and project_override
+    table_util.merge_table(result, config)
 
-    merged_config["framework_setup"] = nil
-    merged_config["project_override"] = nil
+    result["framework_setup"] = nil
+    result["project_override"] = nil
 
-    return merged_config
+    return result
 end
 
 return M
