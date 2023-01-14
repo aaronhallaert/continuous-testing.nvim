cf2ce2521b6a3336ec45d5111b1320a5c26529e8
Aaron Hallaert <hallaertaaron@gmail.com>
2023-01-14 20:56:40 +0100

    Add type signatures to config

--- lua/continuous-testing/config.lua
+++ lua/continuous-testing/config.lua
@@ -1,53 +1,39 @@
 local table_util = require("continuous-testing.utils.table")
+
+---@alias FrameworkSetup table<"ruby" | "javascript", FrameworkConfig>
+
 local M = {}
 
-local config = {}
+---@class CTConfig
+---@field notify boolean
+---@field run_tests_on_setup boolean
+---@field frameworks FrameworkConfig
+local continuous_testing_config = {}
 
+---@type CTConfig
 local DEFAULT_CONFIG = {
     notify = false,
     run_tests_on_setup = true,
 }
 
-local set_default_values = function()
-    for key, value in ipairs(DEFAULT_CONFIG) do
+---Apply default values to the passed config
+---@param config CTConfig
+local set_default_values = function(config)
+    for key, value in pairs(DEFAULT_CONFIG) do
         if config[key] == nil then
             config[key] = value
         end
     end
 end
 
-M.set_user_specific_config = function(user_config)
-    config = user_config
+---@param user_config CTInputConfig
+---@return CTConfig
+local parse_user_config = function(user_config)
+    local result = {} ---@type CTConfig
 
-    set_default_values()
-end
-
--- config structure
---
--- notify = $boolean$,
--- run_tests_on_setup = $boolean$,
--- $filetype$= {
---     test_tool = $"rspec"|"vitest"$,
---     test_cmd = command where %file will be replaced by testfile,
---     root_pattern = $path$
--- },
---
--- TODO: Add the patterns to run on save
-M.check_config = function(_config)
-    -- TODO: implement
-end
-
-M.get_config = function()
-    local result = {}
-    -- start with default config
-    local default_c = table_util.deepcopy_table(DEFAULT_CONFIG)
-    table_util.merge_table(result, default_c)
-
-    -- merge the framework configs (general and project)
-    local framework_config = {}
-
+    local framework_config = {} --@type FrameworkConfig
     local general_framework_config =
-        table_util.deepcopy_table(config.framework_setup)
+        table_util.deepcopy_table(user_config.framework_setup)
     if general_framework_config ~= nil then
         table_util.merge_table(framework_config, general_framework_config)
     end
@@ -55,7 +41,7 @@ M.get_config = function()
     -- determine the key of the project
     local key = "unknown"
     local current_dir = vim.fn.getcwd()
-    for path, _ in pairs(config.project_override) do
+    for path, _ in pairs(user_config.project_override) do
         -- Paths can contain lua magic character '-'
         -- String find does not work with this hyphen, therefore they are subbed
         if
@@ -70,19 +56,30 @@ M.get_config = function()
     end
 
     local project_framework_config =
-        table_util.deepcopy_table(config.project_override[key])
+        table_util.deepcopy_table(user_config.project_override[key])
     if project_framework_config ~= nil then
         table_util.merge_table(framework_config, project_framework_config)
     end
     table_util.merge_table(result, framework_config)
 
-    -- merge all other variables and remove the framework and project_override
-    table_util.merge_table(result, config)
+    user_config["framework_setup"] = nil
+    user_config["project_override"] = nil
 
-    result["framework_setup"] = nil
-    result["project_override"] = nil
+    -- merge all other variables and remove the framework and project_override
+    table_util.merge_table(result, user_config)
 
     return result
+end
+
+---@param user_config CTInputConfig
+M.set_config = function(user_config)
+    continuous_testing_config = parse_user_config(user_config)
+    set_default_values(continuous_testing_config)
+end
+
+--@return CTConfig
+M.get_config = function()
+    return table_util.deepcopy_table(continuous_testing_config)
 end
 
 return M
