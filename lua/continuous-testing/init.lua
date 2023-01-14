00a0a795cfe637758fe8960b2ef1b5142b76dbcc
Aaron Hallaert <hallaertaaron@gmail.com>
2023-01-14 20:56:40 +0100

    Add type signatures to config

--- lua/continuous-testing/init.lua
+++ lua/continuous-testing/init.lua
@@ -3,7 +3,7 @@ local commands = require("continuous-testing.commands")
 local M = {}
 
 M.setup = function(config)
-    require("continuous-testing.config").set_user_specific_config(config or {})
+    require("continuous-testing.config").set_config(config or {})
 
     vim.cmd([[
       hi default ContinuousTestingPassed ctermfg=Green guifg=#96F291
---
+++ lua/continuous-testing/types.lua
@@ -0,0 +1,14 @@
+------------
+-- CONFIG --
+------------
+
+---@class FrameworkConfig
+---@field test_tool string
+---@field test_cmd string
+---@field root_pattern string
+
+---@class CTInputConfig
+---@field notify boolean
+---@field run_tests_on_setup boolean
+---@field framework_setup FrameworkSetup
+---@field project_override table<string, FrameworkSetup>
